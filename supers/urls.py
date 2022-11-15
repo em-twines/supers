58ba0472a577cf3d33dd939e61e4bc630b218d64
emilytwines <emdelbridge@gmail.com>
2022-11-15 12:38:38 -0500

    added diplay all by type function to display_chosen_type + fixed if GET part of same funciton

--- supers/urls.py
+++ supers/urls.py
@@ -3,7 +3,8 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.supers),
+    # path('', views.supers),
+    path('', views.display_chosen_type),
     path('<int:pk>/', views.super_details)
 
 
--- supers/views.py
+++ supers/views.py
@@ -6,25 +6,25 @@ from rest_framework.response import Response
 from .models import Super
 from .serializers import SuperSerializer
 
+
 # Create your views here.
-# path('', views.supers),
 
 
-@api_view(['GET', 'POST'])
-def supers(request):
-    if request.method == 'GET':
-        supers_list = Super.objects.all()
-        serializer = SuperSerializer(supers_list, many = True)
-        return Response(serializer.data, status= status.HTTP_200_OK)
+# @api_view(['GET', 'POST'])
+# def supers(request):
+#     if request.method == 'GET':
+#         supers_list = Super.objects.all()
+#         serializer = SuperSerializer(supers_list, many = True)
+#         return Response(serializer.data, status= status.HTTP_200_OK)
 
-    elif request.method == 'POST':
-        serializer = SuperSerializer(data= request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data, status.HTTP_201_CREATED)
+#     elif request.method == 'POST':
+#         serializer = SuperSerializer(data= request.data)
+#         serializer.is_valid(raise_exception=True)
+#         serializer.save()
+#         return Response(serializer.data, status.HTTP_201_CREATED)
 
-    else: 
-        return Response(serializer.errors,status = status.HTTP_400_BAD_REQUEST)
+#     else: 
+#         return Response(serializer.errors,status = status.HTTP_400_BAD_REQUEST)
 
 
 @api_view(["GET", "PUT", "DELETE"])
@@ -46,14 +46,50 @@ def super_details(request, pk):
         return Response(status = status.HTTP_204_NO_CONTENT)
 
 
-api_view(['GET'])
+@api_view(['GET', 'POST'])
 def display_chosen_type(request):
 
-    supers_list = Super.objects.all()
-    super_param = request.query_params.get('type')
-    if super_param:
-        super_param = supers_list.filter(super_type__type=super_param)
+    if request.method == "GET":
 
-    serializer = SuperSerializer(supers_list, many = True)
-    return Response(serializer.data)
+        supers_list = Super.objects.all()
 
+        super_param = request.query_params.get('type')
+
+        if super_param:
+            super_param = supers_list.filter(super_type__type=super_param)
+            # serializer = SuperSerializer(supers_list, many = True)
+            serializer = SuperSerializer(super_param, many = True)
+
+            return Response(serializer.data, status = status.HTTP_200_OK)
+
+
+        else: 
+            custom_response = {}
+            # for type in super_types:
+            type_super = Super.objects.filter(super_type_id = 1)
+            # gives super objects that are heros. requires serialization
+            type_villain = Super.objects.filter(super_type_id =2)
+
+            for ty in type_super:
+                # takes super object in list of heroes
+                hero_list = SuperSerializer(type_super, many = True)
+                #serializes super objects one at a time
+                custom_response = {
+                    "Heroes" : hero_list.data,
+                }
+            for typ in type_villain:
+                villain_list = SuperSerializer(type_villain, many = True)
+                custom_response.update ({
+                    'Villains' : villain_list.data
+                    })
+
+            return Response(custom_response, status = status.HTTP_200_OK)
+
+    elif request.method == "POST":
+        serializer = SuperSerializer(data= request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status.HTTP_201_CREATED)
+
+    else: 
+        return Response(serializer.errors,status = status.HTTP_400_BAD_REQUEST)
